507941d9da2b2dda36071199e65fed84c12a8a29
emilia <emi1213galarza@gmail.com>
2023-12-26 20:33:27 -0500

    Create react aplication

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState([
+    "One Punch",
+    "Samurai X",
+    "Dragon Ball",
+  ]);
+  return <div>GifExpertApp</div>;
+};
